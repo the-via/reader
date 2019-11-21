5bf0b9b74622881199e77ab3091fd1b6ab3f0fcf
Olivia B <ofbriggs@gmail.com>
2019-11-20 23:48:00 -0500

    Add core-js to polyfill array methods

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+import "core-js";
 import validate from './keyboard.definition.validator';
 export * from './transform';
 export * from './kle-parser';
--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("core-js");
 var keyboard_definition_validator_1 = __importDefault(require("./keyboard.definition.validator"));
 exports.validate = keyboard_definition_validator_1.default;
 __export(require("./transform"));
