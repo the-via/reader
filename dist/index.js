7dedde449626083d847d5a0b53e9ddfcd463ba1a
Olivia Briggs <ofbriggs@gmail.com>
2019-12-26 00:21:15 -0500

    Export validation functions

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,8 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-require("core-js");
-var keyboard_definition_validator_1 = __importDefault(require("./validated-types/keyboard-definition.validator"));
-exports.validate = keyboard_definition_validator_1.default;
+__export(require("./validated-types/keyboard-definition-v2.validator"));
 __export(require("./transform"));
 __export(require("./kle-parser"));
 __export(require("./types"));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import "core-js";
-import validate from './validated-types/keyboard-definition.validator';
+export * from './validated-types/keyboard-definition-v2.validator';
 export * from './transform';
 export * from './kle-parser';
 export * from './types';
-export {validate};
