14772fa93ea5fc658ed49bd0d7963a31209f431e
Olivia Briggs <ofbriggs@gmail.com>
2020-01-06 02:17:39 -0500

    export validation function for VIADefinitionV2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './validated-types/keyboard-definition-v2.validator';
+export {isVIADefinitionV2} from './validated-types/via-definition-v2.validator';
 export * from './transform';
 export * from './kle-parser';
 export * from './types';
